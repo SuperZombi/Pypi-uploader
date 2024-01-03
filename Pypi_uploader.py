523b27b85caab8914606c656b098cb7bb27012a9
Super Zombi <super.zombi.yt@gmail.com>
2024-01-03 23:41:32 +0200

    Aded dependencies

--- Pypi_uploader.py
+++ Pypi_uploader.py
@@ -23,6 +23,12 @@ try:
 except:
 	os.system("pip install wheel")
 
+
+def infinite(start=0):
+	while True:
+		yield start
+		start += 1
+
 sg.theme('DarkAmber')
 window_title = "Pypi uploader"
 
@@ -41,14 +47,32 @@ main = [[sg.T("Select project folder: ", font='16'), sg.FolderBrowse("Browse", t
 		[sg.Checkbox("token", font='12', key="token", enable_events=True)],
 		[sg.T("Your email:          ", font='12'), sg.Input("", size=(26,1), key="email")],
 		[sg.T("Github repository: ", font='12'), sg.Input("", size=(26,1), key="github"), sg.T("(Optional)", text_color="yellow", font='Arial 12')],[sg.T("")],
+		[sg.T("Required Dependencies: ", font='12'), sg.Button(' + ', key="add_dependency")],
+		[sg.Col([], scrollable=True, vertical_scroll_only=True, size=(500, 0), key='-dependencies-')],[sg.T("")],
 		[sg.T("Delete all created files, folders and archives after upload?", font='Segoe 14')],
 		[sg.Radio("Yes", 2, default=False, key="delete_yes", font='Segoe 12'), sg.Radio("No", 2, default=True, key="delete_no", font='Segoe 12')],
 		[sg.Push(), sg.OK("Upload", font='12'), sg.Push()]]
 
 
-layout = [[sg.Column(main, size=(500,600), scrollable=True, vertical_scroll_only=True)]]
+layout = [[sg.Column(main, size=(500,600), scrollable=True, vertical_scroll_only=True, key="__main__")]]
 
-window = sg.Window(window_title, layout)
+window = sg.Window(window_title, layout, finalize=True)
+
+def button_pointer():
+	[el.set_cursor("hand2") for el in window.element_list() if isinstance(el, sg.Button)]
+
+def update_window_height(root, scrollzone):
+	scrollzone.contents_changed()
+	scrollzone.Widget.pack_propagate(0)
+	new_height = scrollzone.TKColFrame.TKFrame.winfo_reqheight()
+	scrollzone.set_size((495, min(180, new_height)))
+	scrollzone.widget.canvas.yview_moveto(1)
+	
+	window_height = root.TKColFrame.TKFrame.winfo_reqheight()
+	root.set_size((500, window_height))
+
+button_pointer()
+dep_index = infinite(1)
 showing = False
 while True:
 	event, values = window.read()
@@ -71,6 +95,23 @@ while True:
 		else:
 			window["password_text"].update("Your password:   ")
 
+	elif event == "add_dependency":
+		item_num = next(dep_index)
+		window.extend_layout(window['-dependencies-'],[[sg.pin(
+			sg.Col([[sg.B(sg.SYMBOL_X, k=('remove_dependency', item_num), border_width=0, button_color=(sg.theme_text_color(), sg.theme_background_color())),
+                     sg.Input("", size=(37,1), k=('dependency', item_num)),
+            ]], k=('dependency_item', item_num)))
+		]])
+		window.refresh()
+		update_window_height(window['__main__'], window['-dependencies-'])
+		button_pointer()
+	
+	elif event[0] == 'remove_dependency':
+		window[('dependency_item', event[1])].update(visible=False)
+		window.refresh()
+		update_window_height(window['__main__'], window['-dependencies-'])
+
+
 	if event == "Upload":
 		break
 
@@ -79,6 +120,7 @@ upload_to_test_pypi = values["test_pypi"]
 username = values["username"]
 password = values["password"]
 userlogin = "__token__" if values["token"] else values["username"]
+dependencies = {value for key, value in values.items() if 'dependency' in key[0] and value}
 
 delete_temp_files = False
 if values["delete_yes"] == True:
@@ -119,6 +161,12 @@ if values["github"] != "":
 	comand += "	url='" + str(values["github"]) + "',\n"
 
 comand += "	packages=['" + str(path[len(path)-1]) + "'],\n"
+
+if len(dependencies) > 0:
+	comand += "	install_requires=["
+	comand += ', '.join(f'"{item}"' for item in dependencies)
+	comand += "],\n"
+
 comand += "	include_package_data=True,\n"
 comand += '	classifiers=[\n		"Programming Language :: Python :: 3",\n		"License :: OSI Approved :: MIT License",\n		"Operating System :: OS Independent",\n	],\n'
 if required:
